c0ef36cfc13b22338005bd9faf7bcbfe12229583
zhangji <873068490qq.com>
2019-02-07 12:50:35 +0800

    lll

---
+++ src/main/java/RiZhi.java
@@ -0,0 +1,18 @@
+import org.apache.log4j.*;
+
+/**
+ * @ClassName RiZhi
+ * @Author ZhangJi
+ * @Date 2019/2/6-8:11
+ **/
+public class RiZhi {
+    private static Logger logger = Logger.getLogger(RiZhi.class.getName());
+
+    public static void main(String[] args) {
+        /*Logger logger = Logger.getLogger(RiZhi.class.getName());
+        logger.info("nihao");*/
+        logger.info("wwww");
+        int i=1/0;
+    }
+
+}
